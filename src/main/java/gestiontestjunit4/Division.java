18dbf676b502fab672638ecacabd520446b8e032
Bilal Fall <bilxadim@gmail.com>
2018-09-07 18:43:07 +0000

    Ajout de la methode d'addition dans le fichier Division.java

--- src/main/java/gestiontestjunit4/Division.java
+++ src/main/java/gestiontestjunit4/Division.java
@@ -10,5 +10,15 @@ public class Division extends Operations{
 		}
 		
 	}
+	
+	public long addition(long a, long b){
+		if(b == 0){
+			throw new IllegalArgumentException("La division par b = 0 est impossible");
+		}else{
+			return a+b;
+		}
+		
+	}
+	 
 
 }
